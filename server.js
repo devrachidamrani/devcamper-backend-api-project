55b76d2084dc178a59bd93ef7cb9bbe6435986a4
Rachid Amrani <rachid3alaoui@gmail.com>
2021-06-03 18:44:24 +0100

    Add morgan 3rd middleware

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
 const bootcampsRouter = require('./routes/bootcamps.router')
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcampsRouter)
