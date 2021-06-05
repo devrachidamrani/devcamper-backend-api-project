f63e5bccd59984828530deba5f7c576e174725d3
Rachid Amrani <rachid3alaoui@gmail.com>
2021-06-05 16:12:27 +0100

    Geospatial query to get bootcamps within radius

--- routes/bootcamps.router.js
+++ routes/bootcamps.router.js
@@ -7,9 +7,11 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
 } = require('../controllers/bootcamps.controllers')
 
 router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
 module.exports = router
