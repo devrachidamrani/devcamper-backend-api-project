6fc1aa29255ab45886661d6e88f701328268c2d4
Rachid Amrani <rachid3alaoui@gmail.com>
2021-06-05 15:42:36 +0100

    Add seeder to populate database with data or destoying it

---
+++ seeder.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const colors = require('colors')
+const dotenv = require('dotenv')
+
+// Load env vars
+dotenv.config({
+  path: './config/config.env',
+})
+
+// Load models
+const Bootcamp = require('./models/Bootcamp')
+
+// Connect to DB
+
+mongoose.connect(process.env.DB_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+)
+
+// Import into DB
+
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps)
+    console.log('Bootcamps data imported...'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany()
+    console.log('Bootcamps data destroyed ...'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+if (process.argv[2] === '-import') {
+  importData()
+} else if (process.argv[2] === '-delete') {
+  deleteData()
+}
