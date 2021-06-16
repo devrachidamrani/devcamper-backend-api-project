2e50c1bed6c816bd793a99b9e140c499f672c51a
devrachidamrani <rachid3alaoui@gmail.com>
2021-06-16 16:16:17 +0100

    Add call for next() in the body of middlware function

--- models/User.js
+++ models/User.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please add a name'],
+    required: [true, "Please add a name"],
   },
   email: {
     type: String,
-    required: [true, 'Please add an email'],
+    required: [true, "Please add an email"],
     unique: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email',
+      "Please add a valid email",
     ],
   },
   role: {
     type: String,
-    enum: ['user', 'publisher'],
-    default: 'user',
+    enum: ["user", "publisher"],
+    default: "user",
   },
   password: {
     type: String,
-    required: [true, 'Please add a password'],
+    required: [true, "Please add a password"],
     minlength: 6,
     select: false,
   },
@@ -36,9 +36,10 @@ const UserSchema = new mongoose.Schema({
 })
 
 // Encrype password using bcrypt before storing it in database
-UserSchema.pre('save', async function (next) {
+UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 // Sign JWT and return
@@ -54,4 +55,4 @@ UserSchema.methods.getSignedJwtToken = function () {
   )
 }
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema)
