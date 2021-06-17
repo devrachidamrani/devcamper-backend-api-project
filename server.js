8a664ce2a147fecd3fef1172140070d98a509f21
devrachidamrani <rachid3alaoui@gmail.com>
2021-06-17 16:57:32 +0100

    Modify font weight in terminal for the running server function

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const path = require("path")
-const express = require("express")
-const dotenv = require("dotenv")
-const morgan = require("morgan")
-const connectDB = require("./config/db")
-const colors = require("colors")
-const errorHandler = require("./middlewares/error")
-const fileupload = require("express-fileupload")
-const cookieParser = require("cookie-parser")
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+const colors = require('colors')
+const errorHandler = require('./middlewares/error')
+const fileupload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
 
 // Load env vars
-dotenv.config({ path: "./config/config.env" })
+dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
-const bootcampsRouter = require("./routes/bootcamps.router")
-const coursesRouter = require("./routes/courses.router")
-const authRouter = require("./routes/auth.router")
+const bootcampsRouter = require('./routes/bootcamps.router')
+const coursesRouter = require('./routes/courses.router')
+const authRouter = require('./routes/auth.router')
 
 // Dev logging middleware
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
 
 // Body parser
@@ -29,15 +29,15 @@ app.use(express.json())
 app.use(cookieParser())
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // File uploading
 app.use(fileupload())
 
 // Mount routers
-app.use("/api/v1/bootcamps", bootcampsRouter)
-app.use("/api/v1/courses", coursesRouter)
-app.use("/api/v1/auth", authRouter)
+app.use('/api/v1/bootcamps', bootcampsRouter)
+app.use('/api/v1/courses', coursesRouter)
+app.use('/api/v1/auth', authRouter)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
@@ -49,8 +49,8 @@ const startServer = () => {
       app.listen(
         PORT,
         console.log(
-          `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.black
-            .bgMagenta
+          `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.white
+            .bold.bgMagenta
         )
       )
     })
